f24a6a81c2a59bf4070c50e6c53aef3815cae25e
phillipmont <phillip.monteiro5@gmail.com>
2024-12-13 13:52:13 -0500

    Added interact panel

--- Assets/Scripts/Items/BaseItem.cs
+++ Assets/Scripts/Items/BaseItem.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public enum Rarity
@@ -40,7 +39,7 @@ public abstract class BaseItem : MonoBehaviour
 
         StartCoroutine(InteractPanelRoutine());
 
-        Destroy(gameObject);
+        GetComponent<SpriteRenderer>().enabled = false;
     }
 
     // Optional: Item Specific Behavior
@@ -57,18 +56,15 @@ public abstract class BaseItem : MonoBehaviour
         GameObject panel = interactPanel.transform.GetChild(0).gameObject;
         
         panel.gameObject.SetActive(true);
-        panel.transform.GetChild(0).GetComponent<Image>().sprite = itemData.itemSprite;
+        panel.transform.GetChild(6).GetComponent<Image>().sprite = itemData.itemSprite;
         panel.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = itemData.itemName;
-        panel.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = itemData.itemRarity.ToString();
-        panel.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = itemData.itemValue.ToString();
+        panel.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = itemData.itemRarity.ToString();
+        panel.transform.GetChild(5).GetComponent<TextMeshProUGUI>().text = itemData.itemValue.ToString();
 
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(3.0f);
 
         panel.gameObject.SetActive(false);
 
-        //interactPanel.transform.GetChild(0).GetComponent<Image>().enabled = false;
-        //interactPanel.transform.GetChild(1).GetComponent<TextMeshProUGUI>().enabled = false;
-        //interactPanel.transform.GetChild(2).GetComponent<TextMeshProUGUI>().enabled = false;
-        //interactPanel.transform.GetChild(3).GetComponent<TextMeshProUGUI>().enabled = false;
+        Destroy(gameObject);
     }
 }
